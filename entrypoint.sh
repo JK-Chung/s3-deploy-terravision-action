b923a1fc1f68c2c32a831285840b7bbb80e1fb24
JK-Chung <chungjeeken@outlook.com>
2023-06-03 19:14:43 +0100

    Added code that should upload the terravision graphdata onto an S3 bucket

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,13 @@
 #!/bin/bash -l
-terravision
-aws --version
+AWS_ACCESS_KEY_ID="$1"
+AWS_SECRET_ACCESS_KEY="$2"
+AWS_S3_BUCKET_BASE_URI="$3"
+
+cd /github/workspace
+
+ls
+
+terravision graphdata --outfile architecture.json
+cat architecture.json
+
+aws s3 cp architecture.json $AWS_S3_BUCKET_BASE_URI/architecture.json
