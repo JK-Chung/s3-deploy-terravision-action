746213ba1f2af31b29a8f5107c0582a2a4900281
JK-Chung <chungjeeken@outlook.com>
2023-06-03 19:31:27 +0100

    Correcting CLI command for AWS cp

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,4 +11,4 @@ ls
 terravision graphdata --outfile architecture.json
 cat architecture.json
 
-aws s3 cp architecture.json $AWS_S3_BUCKET_BASE_URI/architecture.json
+aws s3 cp architecture.json $AWS_S3_BUCKET_BASE_URI/architecture.json/
